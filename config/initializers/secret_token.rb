0d7fee5895c488cff50cd637617bdb0eda886413
Christian <moscardi79@gmail.com>
2011-12-28 18:25:04 -0500

    First commit for the rails tutorial. Who knew you could write such a message in emacs?

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'ea68fb186a52a227ddfdf6bd038d8894cc969ba4d23ec760b4b210b215e1194289451237be55537afc453d96fb3bd165d5b5d1383b57e4120f64e7b6adac2317'
